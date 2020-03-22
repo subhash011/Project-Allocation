b17c6f7e45a4bf6067e0a661e36d4070b94ef299
subhash011 <subhash011011@gmail.com>
2020-03-22 17:14:38 +0530

    small changes

--- backend/models/Faculty.js
+++ backend/models/Faculty.js
@@ -1,31 +1,31 @@
-const mongoose=require('mongoose');
+const mongoose = require("mongoose");
 
-const UserSchema=new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
     },
-    email:{
-        type:String,
-        required:true
+    email: {
+        type: String,
+        required: true
     },
-    isAdmin:{
-        type:Boolean
+    isAdmin: {
+        type: Boolean
     },
-    stream:{
-        type:String,
-        required:true
+    stream: {
+        type: String,
+        required: true
     },
-    project_list:{
-        type:[]
+    project_list: {
+        type: []
     },
-    student_preference:{
-        type:[]
+    student_preference: {
+        type: []
     },
-    date:{
-        type:Date,
-        default:Date.now
-     }
-})
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
 
-module.exports=mongoose.model('Faculty',UserSchema);
+module.exports = mongoose.model("Faculty", UserSchema);
