4a3693f8e2f60692b5d844887fbc6cdd0728c368
subhash011 <subhash011011@gmail.com>
2020-03-22 16:46:23 +0530

    added basic auth with additional node modules

--- backend/models/Faculty.js
+++
@@ -1,7 +0,0 @@
-const mongoose=require('mongoose');
-
-const UserSchema=new mongoose.Schema({
-
-})
-
-module.exports=mongoose.model('Faculty',UserSchema);
--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,22 @@
 //imports
 const express = require("express");
-const path = require("path");
+const app = express();
 const mongoose = require("mongoose");
 const bodyparser = require("body-parser");
-<<<<<<< HEAD
 const flash = require("connect-flash");
-=======
-const cors = require('cors')
+const cors = require("cors");
 
+//start server
 const app = express();
 
 //Using cors to enable request from thrid party api's
 app.use(cors());
 
->>>>>>> vamsi
 //use body-parser
 app.use(bodyparser.json());
 
 //connect to mongodb
 mongoose
-<<<<<<< HEAD
     .connect("mongodb://localhost:27017/ProjectAllocationTest", {
         useNewUrlParser: true,
         useUnifiedTopology: true
@@ -30,34 +27,23 @@ mongoose
     .catch(err => {
         console.log(err);
     });
+
 //define all routes below this
 const auth = require("./gmail/index");
 const home = require("./routes/home");
 app.use("/", home);
 app.use("/auth", auth);
-=======
-  .connect("mongodb://localhost:27017/ProjectAllocationTest", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => {
-    console.log("Connected to mongodb");
-  })
-  .catch(err => {
-    console.log(err);
-  });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 //Error Response for routes not registered
 app.get("*", (req, res) => {
-  res.status(404).json({
-      error: "Page Not found"
-  });
+    res.status(404).json({
+        error: "Page Not found"
+    });
 });
 
 //start server
-app.listen(PORT, () => {
-  console.log("Server connected to port " + PORT);
+app.listen(8080, () => {
+    console.log("connected to server");
 });
->>>>>>> vamsi
