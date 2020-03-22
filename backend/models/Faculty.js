f0c6223d4a282cc2578580d90c1d47659c33ee22
Vamsi995 <saivamsi.ds123@gmail.com>
2020-03-22 16:53:12 +0530

    Populating models

--- backend/models/Faculty.js
+++ backend/models/Faculty.js
@@ -1,7 +1,31 @@
 const mongoose=require('mongoose');
 
 const UserSchema=new mongoose.Schema({
-
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    isAdmin:{
+        type:Boolean
+    },
+    stream:{
+        type:String,
+        required:true
+    },
+    project_list:{
+        type:[]
+    },
+    student_preference:{
+        type:[]
+    },
+    date:{
+        type:Date,
+        default:Date.now
+     }
 })
 
 module.exports=mongoose.model('Faculty',UserSchema);
--- backend/models/Project.js
+++ backend/models/Project.js
@@ -1,7 +1,15 @@
 const mongoose=require('mongoose');
 
 const UserSchema=new mongoose.Schema({
-
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    
 })
 
 module.exports=mongoose.model('Project',UserSchema);
