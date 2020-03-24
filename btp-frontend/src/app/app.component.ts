f1d44f5e179fca45543b05f003f5b62ff8e6fa8d
subhash011 <subhash011011@gmail.com>
2020-03-25 00:18:26 +0530

    completed navbar

--- btp-frontend/src/app/app.component.ts
+++ btp-frontend/src/app/app.component.ts
@@ -1,11 +1,24 @@
-import { Component } from "@angular/core";
-
+import { Component, OnInit } from "@angular/core";
+import { MatIconRegistry } from "@angular/material/icon";
+import { DomSanitizer } from "@angular/platform-browser";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = "btp-frontend";
   login = true;
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ) {}
+  ngOnInit() {
+    this.matIconRegistry.addSvgIcon(
+      "iit_pkd",
+      this.domSanitizer.bypassSecurityTrustResourceUrl(
+        "../../../../../../images/IITPKD_Logo.svg"
+      )
+    );
+  }
 }
--- btp-frontend/src/app/components/navbar/navbar.component.ts
+++ btp-frontend/src/app/components/navbar/navbar.component.ts
@@ -2,7 +2,6 @@ import { UserService } from "./../../services/user/user.service";
 import { Router } from "@angular/router";
 import { ActivatedRoute } from "@angular/router";
 import { Component, OnInit } from "@angular/core";
-
 @Component({
   selector: "app-navbar",
   templateUrl: "./navbar.component.html",
@@ -10,7 +9,7 @@ import { Component, OnInit } from "@angular/core";
 })
 export class NavbarComponent implements OnInit {
   constructor(private router: Router, private userService: UserService) {}
-  role: string = "student";
+  role: string = "faculty";
   ngOnInit() {
     if (this.userService) {
       this.role = this.userService.role.toString();
