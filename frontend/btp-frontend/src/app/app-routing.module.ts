dc7cf037695fe6459ccb3142636b5ac2ce4fd487
subhash011 <subhash011011@gmail.com>
2020-03-24 17:34:24 +0530

    none

--- frontend/btp-frontend/src/app/app-routing.module.ts
+++ frontend/btp-frontend/src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { LoginComponent } from "./login/login.component";
+import { LoginComponent } from "./components/login/login.component";
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
