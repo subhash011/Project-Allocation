3d752ef768f092d3291a2c167570637ae9e4223e
subhash011 <subhash011011@gmail.com>
2020-03-25 15:39:00 +0530

    started student

--- btp-frontend/src/app/components/student/student.component.ts
+++ btp-frontend/src/app/components/student/student.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { UserService } from "src/app/services/user/user.service";
 
 @Component({
-  selector: 'app-student',
-  templateUrl: './student.component.html',
-  styleUrls: ['./student.component.css']
+  selector: "app-student",
+  templateUrl: "./student.component.html",
+  styleUrls: ["./student.component.css"]
 })
 export class StudentComponent implements OnInit {
+  constructor(private userService: UserService) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
