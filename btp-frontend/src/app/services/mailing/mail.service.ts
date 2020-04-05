24bed6fbdaab45de65a4701bc90ddc9f64a05f87
Vamsi995 <saivamsi.ds123@gmail.com>
2020-04-05 16:47:05 +0530

    mail body change

--- btp-frontend/src/app/services/mailing/mail.service.ts
+++ btp-frontend/src/app/services/mailing/mail.service.ts
@@ -110,14 +110,10 @@ ${stream} Admin
     return this.http.post(url, body, httpOptions);
   }
 
-  formatAMPM(date) {
+  formatAMPM(curr_date) {
+    const date = new Date(curr_date)
     var hours = date.getHours();
-    var minutes = date.getMinutes();
     var ampm = hours >= 12 ? 'pm' : 'am';
-    hours = hours % 12;
-    hours = hours ? hours : 12; // the hour '0' should be '12'
-    minutes = minutes < 10 ? '0'+minutes : minutes;
-    var strTime = hours + ':' + minutes + ' ' + ampm;
     return ampm;
   }
 
