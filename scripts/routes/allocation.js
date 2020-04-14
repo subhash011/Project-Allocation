3eaaa6204e15ff5c764b4b414a3dde7db2b19301
subhash011 <subhash011011@gmail.com>
2020-04-14 20:13:03 +0530

    done with the allocation

--- scripts/routes/allocation.js
+++ scripts/routes/allocation.js
@@ -5,54 +5,41 @@ const Student = require("../models/Student");
 const Project = require("../models/Project");
 const Faculty = require("../models/Faculty");
 
-router.post("/combine", (req, res) => {
-    var promises = [];
-    var students = [];
-    var projects = [];
-    promises.push(
-        Student.find().then((student) => {
-            student.sort((a, b) => {
-                return b.gpa - a.gpa;
-            });
-            return student;
-        })
-    );
-    promises.push(
-        Project.find().then((project) => {
-            project.sort((a, b) => {
-                return a.students_id.length - b.students_id.length;
-            });
-            return project;
-        })
-    );
-    Promise.all(promises).then((result) => {
-        students = result[0];
-        projects = result[1];
-        studentIDS = students.map((val) => val._id);
-        projectIDS = projects.map((val) => val._id);
-        for (const project of projects) {
-            const setA = new Set(project.students_id.map((val) => val.toString()));
-            const setB = new Set(studentIDS.map((val) => val.toString()));
-            const union = new Set([...setA, ...setB]);
-            project.students_id = [...union];
-            project.students_id = project.students_id.map((val) =>
-                mongoose.Types.ObjectId(val)
-            );
-        }
-        for (const student of students) {
-            const setA = new Set(
-                student.projects_preference.map((val) => val.toString())
-            );
-            const setB = new Set(projectIDS.map((val) => val.toString()));
-            const union = new Set([...setA, ...setB]);
-            student.projects_preference = [...union];
-            student.projects_preference = student.projects_preference.map((val) =>
-                mongoose.Types.ObjectId(val)
-            );
-        }
-        res.json(students);
-    });
-});
+function combineProjects(projects, students) {
+    students = students;
+    projects = projects;
+    studentIDS = students.map((val) => val._id);
+    projectIDS = projects.map((val) => val._id);
+    for (const project of projects) {
+        const setA = new Set(project.students_id.map((val) => val.toString()));
+        const setB = new Set(studentIDS.map((val) => val.toString()));
+        const union = new Set([...setA, ...setB]);
+        project.students_id = [...union];
+        project.students_id = project.students_id.map((val) =>
+            mongoose.Types.ObjectId(val)
+        );
+    }
+    return projects;
+}
+
+function combineStudents(projects, students) {
+    students = students;
+    projects = projects;
+    studentIDS = students.map((val) => val._id);
+    projectIDS = projects.map((val) => val._id);
+    for (const student of students) {
+        const setA = new Set(
+            student.projects_preference.map((val) => val.toString())
+        );
+        const setB = new Set(projectIDS.map((val) => val.toString()));
+        const union = new Set([...setA, ...setB]);
+        student.projects_preference = [...union];
+        student.projects_preference = student.projects_preference.map((val) =>
+            mongoose.Types.ObjectId(val)
+        );
+    }
+    return students;
+}
 
 router.post("/start", (req, res) => {
     var projects = [];
@@ -62,9 +49,7 @@ router.post("/start", (req, res) => {
     var allocationStatus = {};
     var promises = [];
     promises.push(
-        Project.find()
-        .populate("students_id", null, Student)
-        .then((projectList) => {
+        Project.find().then((projectList) => {
             projects = projectList;
             projects.sort((b, a) => {
                 return a.students_id.length - b.students_id.length;
@@ -73,44 +58,46 @@ router.post("/start", (req, res) => {
         })
     );
     promises.push(
-        Student.find()
-        .populate("projects_preference", null, Project)
-        .then((studentList) => {
+        Student.find().then((studentList) => {
             students = studentList;
             students.sort((a, b) => {
                 return a.gpa - b.gpa;
             });
-            free = [...students];
             return students;
         })
     );
     Promise.all(promises).then(() => {
-        var curStudent, firstPreference;
+        combineStudents(projects, students);
+        combineProjects(projects, students);
+        free = [...students];
+        var curStudent, firstPreference, firstProject;
         while (free.length > 0) {
             curStudent = free[0];
             firstPreference = curStudent.projects_preference[0];
+            firstProject = projects.find((val) => {
+                return val.equals(firstPreference.toString());
+            });
             if (!firstPreference) {
                 free.shift();
                 curStudent = free[0];
                 firstPreference = curStudent.projects_preference[0];
             }
-            if (!allocationStatus[firstPreference._id]) {
-                allocationStatus[firstPreference._id] = [];
-                allocationStatus[firstPreference._id].push(curStudent);
+            if (!allocationStatus[firstPreference]) {
+                allocationStatus[firstPreference] = [];
+                allocationStatus[firstPreference].push(curStudent);
                 free = free.filter((val) => {
                     return !val.equals(curStudent);
                 });
                 alloted.push(curStudent);
             } else {
                 if (
-                    allocationStatus[firstPreference._id].length <
-                    firstPreference.studentIntake
+                    allocationStatus[firstPreference].length < firstProject.studentIntake
                 ) {
-                    allocationStatus[firstPreference._id].push(curStudent);
-                    allocationStatus[firstPreference._id].sort((a, b) => {
+                    allocationStatus[firstPreference].push(curStudent);
+                    allocationStatus[firstPreference].sort((a, b) => {
                         return (
-                            firstPreference.students_id.indexOf(a) -
-                            firstPreference.students_id.indexOf(b)
+                            firstProject.students_id.indexOf(a) -
+                            firstProject.students_id.indexOf(b)
                         );
                     });
                     free = free.filter((val) => {
@@ -122,15 +109,15 @@ router.post("/start", (req, res) => {
                         allocationStatus[firstPreference._id][
                             allocationStatus[firstPreference._id].length - 1
                         ];
-                    var currentlyAllotedIndex = firstPreference.students_id.indexOf(
+                    var currentlyAllotedIndex = firstProject.students_id.indexOf(
                         studentCurrentlyAlloted._id
                     );
-                    var curStudentIndex = firstPreference.students_id.indexOf(
+                    var curStudentIndex = firstProject.students_id.indexOf(
                         curStudent._id
                     );
                     if (curStudentIndex < currentlyAllotedIndex) {
-                        allocationStatus[firstPreference._id].pop();
-                        allocationStatus[firstPreference._id].push(curStudent);
+                        allocationStatus[firstPreference].pop();
+                        allocationStatus[firstPreference].push(curStudent);
                         free = free.filter((val) => {
                             return !val.equals(curStudent);
                         });
