44120e583045c20ee1edec42d456ba54d9c45fbd
subhash011 <subhash011011@gmail.com>
2020-04-14 09:39:54 +0530

    done

--- scripts/server.js
+++ scripts/server.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyparser = require("body-parser");
-require("dotenv/config");
 
 // start the server
 const session = require("express-session");
