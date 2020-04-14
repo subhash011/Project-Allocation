26a808e65685aeddebd871e64e171ccb06176fc0
Vamsi995 <saivamsi.ds123@gmail.com>
2020-04-14 11:59:58 +0530

    changes

--- scripts/routes/project.js
+++ scripts/routes/project.js
@@ -22,7 +22,7 @@ router.post('/:faculty/:num',(req,res)=>{
                 
          for (let index = 0; index < num; index++) {
                 let names = faculty_name +"_p" + (index + 1);
-                let studentIntake = (Math.random()*5 + 5).toFixed()
+                let studentIntake = (Math.random()*5 + 1).toFixed()
 
                 const project = new Project({
                     title: names,
@@ -58,6 +58,90 @@ router.post('/:faculty/:num',(req,res)=>{
             console.log(err)
         })
 
+
+
+
+})
+
+
+router.post("/add",(req,res)=>{
+
+
+    var promises = [];
+
+
+    Faculty.find({})
+        .then(faculties=>{
+
+
+            for(const faculty of faculties){
+
+
+                const num = (Math.random()*5 + 1).toFixed();
+
+                for (let index = 0; index < num; index++) {
+                    let names = faculty.name +"_p" + (index + 1);
+                    let studentIntake = (Math.random()*5 + 1).toFixed()
+            
+                    const project = new Project({
+                        title: names,
+                        studentIntake: studentIntake,
+                        faculty_id:faculty._id,
+                    })
+                
+                    faculty.project_list.push(project._id);
+            
+                    promises.push(
+                        project.save().then((result) => {
+                                    return result;
+                            
+                        })
+                        .catch(err=>{
+                            console.log(err);
+                        })
+                    );
+                }
+
+
+                promises.push(
+                    faculty.save()
+                        .then(result=>{
+                            return result;
+                        })
+                        .catch(err=>{
+                            console.log(err)
+                        })
+                )
+
+
+
+            }
+
+
+            Promise.all(promises)
+            .then(result=>{
+               res.json(result);
+            })
+            .catch(err=>{
+                console.log(err)
+            })
+
+
+
+
+
+
+
+
+
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+
+
+            
+ 
 
 
 
