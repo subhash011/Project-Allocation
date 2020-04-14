aef63cc3ad9bd5bca0c8ce454559b4ec7a275c5b
subhash011 <subhash011011@gmail.com>
2020-04-14 09:34:22 +0530

    added scripts folder

---
+++ scripts/models/Student.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    gpa: {
+        type: Number,
+        required: true,
+    },
+    projects_preference: {
+        type: [mongoose.SchemaTypes.ObjectId],
+    },
+    project_alloted: {
+        type: mongoose.SchemaTypes.ObjectId,
+    },
+});
+
+module.exports = mongoose.model("Student", UserSchema);
