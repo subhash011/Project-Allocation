38b9ec76d9afb4bdc36aa046bf1dfe2214673747
subhash011 <subhash011011@gmail.com>
2020-04-16 15:49:27 +0530

    done

--- btp-frontend/src/app/components/shared/timeline/timeline.component.ts
+++ btp-frontend/src/app/components/shared/timeline/timeline.component.ts
@@ -38,6 +38,7 @@ export class TimelineComponent implements OnInit, OnChanges {
   stageThree: number;
   stageFour: number;
   message: String;
+  next: String;
   icon;
   ngOnChanges(changes: SimpleChanges) {
     this.program = changes.program.currentValue;
@@ -50,115 +51,110 @@ export class TimelineComponent implements OnInit, OnChanges {
         "margin-top.%": "3",
         left: "0",
       };
+      this.stream = this.program;
       this.userService
-        .getStudentDetails(localStorage.getItem("id"))
+        .getAllAdminDetails()
         .toPromise()
-        .then((student) => {
-          if (student["status"] == "success") {
-            this.stream = student["user_details"]["stream"];
-            return this.stream;
-          }
-        })
-        .then((stream) => {
-          this.userService
-            .getAllAdminDetails()
-            .toPromise()
-            .then((result) => {
-              if (result["message"] == "success") {
-                this.admins = result["result"][stream];
-                if (result["result"] && result["result"][stream]) {
-                  if (this.admins.startDate) {
-                    this.curDeadline = new Date(
-                      this.admins.deadlines[this.admins.deadlines.length - 1]
-                    );
-                    this.startDate = new Date(this.admins.startDate);
-                    this.startCompleted = true;
-                    this.stage = this.admins.stage;
-                    for (let i = 0; i <= this.stage; i++) {
-                      this.dates.push(new Date(this.admins["deadlines"][i]));
-                      if (this.stage == 0 && i == 0) {
-                        this.message =
-                          "Faculties add projects during this period";
-                        const now = new Date();
-                        this.stageOne =
-                          Math.abs(now.getTime() - this.startDate.getTime()) /
-                          Math.abs(
-                            this.dates[0].getTime() - this.startDate.getTime()
-                          );
-                        this.stageOne = this.stageOne * 100;
-                        if (this.stageOne >= 100) {
-                          this.stageOneCompleted = true;
-                        }
-                      }
-                      if (this.stage == 1 && i == 1) {
-                        this.message =
-                          "You have to fill in your preferences during this period";
-                        this.stageOneCompleted = true;
-                        this.stageOne = 100;
-                        const now = new Date();
-                        this.stageTwo =
-                          Math.abs(now.getTime() - this.dates[0].getTime()) /
-                          Math.abs(
-                            this.dates[1].getTime() - this.dates[0].getTime()
-                          );
-                        this.stageTwo *= 100;
-                        if (this.stageTwo >= 100) {
-                          this.stageTwoCompleted = true;
-                        }
-                      }
-                      if (this.stage == 2 && i == 2) {
-                        this.message =
-                          "Faculties start giving their preferences of students for their projects";
-                        this.stageOneCompleted = true;
-                        this.stageOne = 100;
-                        this.stageTwoCompleted = true;
-                        this.stageTwo = 100;
-                        const now = new Date();
-                        this.stageThree =
-                          Math.abs(now.getTime() - this.dates[1].getTime()) /
-                          Math.abs(
-                            this.dates[2].getTime() - this.dates[1].getTime()
-                          );
-                        this.stageThree *= 100;
-                        if (this.stageThree >= 100) {
-                          this.stageThreeCompleted = true;
-                        }
-                      }
-                      if (this.stage == 3 && i + 1 == 4) {
-                        this.message =
-                          "Project allocation will be done within this period";
-                        this.stageThreeCompleted = true;
-                        this.stageThree = 100;
-                        this.stageOneCompleted = true;
-                        this.stageOne = 100;
-                        this.stageTwoCompleted = true;
-                        this.stageTwo = 100;
-                        const now = new Date();
-                        this.stageFour =
-                          Math.abs(now.getTime() - this.dates[2].getTime()) /
-                          Math.abs(
-                            this.dates[3].getTime() - this.dates[2].getTime()
-                          );
-                        this.stageFour *= 100;
-                        if (this.stageFour >= 100) {
-                          this.stageFourCompleted = true;
-                        }
-                      }
-                      if (this.stage == 4 && i == 4) {
-                        this.stageThreeCompleted = true;
-                        this.stageThree = 100;
-                        this.stageOneCompleted = true;
-                        this.stageOne = 100;
-                        this.stageTwoCompleted = true;
-                        this.stageTwo = 100;
-                        this.stageFourCompleted = true;
-                        this.stageFour = 100;
-                      }
+        .then((result) => {
+          if (result["message"] == "success") {
+            this.admins = result["result"][this.stream];
+            if (result["result"] && result["result"][this.stream]) {
+              if (this.admins.startDate) {
+                this.curDeadline = new Date(
+                  this.admins.deadlines[this.admins.deadlines.length - 1]
+                );
+                this.startDate = new Date(this.admins.startDate);
+                this.startCompleted = true;
+                this.stage = this.admins.stage;
+                for (let i = 0; i <= this.stage; i++) {
+                  this.dates.push(new Date(this.admins["deadlines"][i]));
+                  if (this.stage == 0 && i == 0) {
+                    this.message = "Faculties add projects during this period";
+                    this.next =
+                      "You have to fill in your preferences during this period";
+                    const now = new Date();
+                    this.stageOne =
+                      Math.abs(now.getTime() - this.startDate.getTime()) /
+                      Math.abs(
+                        this.dates[0].getTime() - this.startDate.getTime()
+                      );
+                    this.stageOne = this.stageOne * 100;
+                    if (this.stageOne >= 100) {
+                      this.stageOneCompleted = true;
                     }
+                  }
+                  if (this.stage == 1 && i == 1) {
+                    this.message =
+                      "You have to fill in your preferences during this period";
+                    this.next =
+                      "Faculties start giving their preferences of students for their projects";
+                    this.stageOneCompleted = true;
+                    this.stageOne = 100;
+                    const now = new Date();
+                    this.stageTwo =
+                      Math.abs(now.getTime() - this.dates[0].getTime()) /
+                      Math.abs(
+                        this.dates[1].getTime() - this.dates[0].getTime()
+                      );
+                    this.stageTwo *= 100;
+                    if (this.stageTwo >= 100) {
+                      this.stageTwoCompleted = true;
+                    }
+                  }
+                  if (this.stage == 2 && i == 2) {
+                    this.message =
+                      "Faculties start giving their preferences of students for their projects";
+                    this.next =
+                      "Project allocation will be done within this period";
+                    this.stageOneCompleted = true;
+                    this.stageOne = 100;
+                    this.stageTwoCompleted = true;
+                    this.stageTwo = 100;
+                    const now = new Date();
+                    this.stageThree =
+                      Math.abs(now.getTime() - this.dates[1].getTime()) /
+                      Math.abs(
+                        this.dates[2].getTime() - this.dates[1].getTime()
+                      );
+                    this.stageThree *= 100;
+                    if (this.stageThree >= 100) {
+                      this.stageThreeCompleted = true;
+                    }
+                  }
+                  if (this.stage == 3 && i + 1 == 4) {
+                    this.message =
+                      "Project allocation will be done within this period";
+                    this.stageThreeCompleted = true;
+                    this.stageThree = 100;
+                    this.stageOneCompleted = true;
+                    this.stageOne = 100;
+                    this.stageTwoCompleted = true;
+                    this.stageTwo = 100;
+                    const now = new Date();
+                    this.stageFour =
+                      Math.abs(now.getTime() - this.dates[2].getTime()) /
+                      Math.abs(
+                        this.dates[3].getTime() - this.dates[2].getTime()
+                      );
+                    this.stageFour *= 100;
+                    if (this.stageFour >= 100) {
+                      this.stageFourCompleted = true;
+                    }
+                  }
+                  if (this.stage == 4 && i == 4) {
+                    this.stageThreeCompleted = true;
+                    this.stageThree = 100;
+                    this.stageOneCompleted = true;
+                    this.stageOne = 100;
+                    this.stageTwoCompleted = true;
+                    this.stageTwo = 100;
+                    this.stageFourCompleted = true;
+                    this.stageFour = 100;
                   }
                 }
               }
-            });
+            }
+          }
         });
     } else {
       this.icon = {
