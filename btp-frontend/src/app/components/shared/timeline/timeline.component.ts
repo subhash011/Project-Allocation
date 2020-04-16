0a60881829c673ee4e8cd86ffb85d0c3823e4e92
subhash011 <subhash011011@gmail.com>
2020-04-16 15:25:51 +0530

    done

--- btp-frontend/src/app/components/shared/timeline/timeline.component.ts
+++ btp-frontend/src/app/components/shared/timeline/timeline.component.ts
@@ -6,6 +6,8 @@ import {
   AfterViewInit,
   TemplateRef,
   ChangeDetectorRef,
+  OnChanges,
+  SimpleChanges,
 } from "@angular/core";
 
 @Component({
@@ -13,7 +15,7 @@ import {
   templateUrl: "./timeline.component.html",
   styleUrls: ["./timeline.component.scss"],
 })
-export class TimelineComponent implements OnInit {
+export class TimelineComponent implements OnInit, OnChanges {
   @Input() program;
   constructor(
     private userService: UserService,
@@ -37,6 +39,10 @@ export class TimelineComponent implements OnInit {
   stageFour: number;
   message: String;
   icon;
+  ngOnChanges(changes: SimpleChanges) {
+    this.program = changes.program.currentValue;
+    this.ngOnInit();
+  }
   ngOnInit() {
     if (localStorage.getItem("role") == "student") {
       this.icon = {
@@ -161,110 +167,98 @@ export class TimelineComponent implements OnInit {
         left: "0",
       };
       this.userService
-        .getFacultyDetails(localStorage.getItem("id"))
+        .getAllAdminDetails()
         .toPromise()
-        .then((faculty) => {
-          if (faculty["status"] == "success") {
-            this.stream = faculty["user_details"]["stream"];
-            return this.stream;
-          }
-        })
-        .then((stream) => {
-          this.userService
-            .getAllAdminDetails()
-            .toPromise()
-            .then((result) => {
-              if (result["message"] == "success") {
-                stream = this.program;
-                if (result["result"] && result["result"][stream]) {
-                  this.admins = result["result"][this.program];
-                  if (this.admins.startDate) {
-                    this.curDeadline = new Date(
-                      this.admins.deadlines[this.admins.deadlines.length - 1]
-                    );
-                    this.startDate = new Date(this.admins.startDate);
-                    this.startCompleted = true;
-                    this.stage = this.admins.stage;
-                    for (let i = 0; i <= this.stage; i++) {
-                      this.dates.push(new Date(this.admins["deadlines"][i]));
-                      if (this.stage == 0 && i == 0) {
-                        const now = new Date();
-                        this.message =
-                          "Faculties add projects during this period";
-                        this.stageOne =
-                          Math.abs(now.getTime() - this.startDate.getTime()) /
-                          Math.abs(
-                            this.dates[0].getTime() - this.startDate.getTime()
-                          );
-                        this.stageOne = this.stageOne * 100;
-                        if (this.stageOne >= 100) {
-                          this.stageOneCompleted = true;
-                        }
-                      }
-                      if (this.stage == 1 && i == 1) {
-                        this.stageOneCompleted = true;
-                        this.stageOne = 100;
-                        const now = new Date();
-                        this.stageTwo =
-                          Math.abs(now.getTime() - this.dates[0].getTime()) /
-                          Math.abs(
-                            this.dates[1].getTime() - this.dates[0].getTime()
-                          );
-                        this.stageTwo *= 100;
-                        if (this.stageTwo >= 100) {
-                          this.stageTwoCompleted = true;
-                        }
-                      }
-                      if (this.stage == 2 && i == 2) {
-                        this.stageOneCompleted = true;
-                        this.stageOne = 100;
-                        this.stageTwoCompleted = true;
-                        this.stageTwo = 100;
-                        const now = new Date();
-                        this.stageThree =
-                          Math.abs(now.getTime() - this.dates[1].getTime()) /
-                          Math.abs(
-                            this.dates[2].getTime() - this.dates[1].getTime()
-                          );
-                        this.stageThree *= 100;
-                        if (this.stageThree >= 100) {
-                          this.stageThreeCompleted = true;
-                        }
-                      }
-
-                      if (this.stage == 3 && i + 1 == 4) {
-                        this.stageThreeCompleted = true;
-                        this.stageThree = 100;
-                        this.stageOneCompleted = true;
-                        this.stageOne = 100;
-                        this.stageTwoCompleted = true;
-                        this.stageTwo = 100;
-                        const now = new Date();
-                        this.stageFour =
-                          Math.abs(now.getTime() - this.dates[2].getTime()) /
-                          Math.abs(
-                            this.dates[3].getTime() - this.dates[2].getTime()
-                          );
-                        this.stageFour *= 100;
-                        if (this.stageFour >= 100) {
-                          this.stageFourCompleted = true;
-                        }
-                      }
-                      if (this.stage == 4 && i == 4) {
-                        this.stageThreeCompleted = true;
-                        this.stageThree = 100;
-                        this.stageOneCompleted = true;
-                        this.stageOne = 100;
-                        this.stageTwoCompleted = true;
-                        this.stageTwo = 100;
-                        this.stageFourCompleted = true;
-                        this.stageFour = 100;
-                      }
+        .then((result) => {
+          if (result["message"] == "success") {
+            this.stream = this.program;
+            if (result["result"] && result["result"][this.stream]) {
+              this.admins = result["result"][this.program];
+              if (this.admins.startDate) {
+                this.curDeadline = new Date(
+                  this.admins.deadlines[this.admins.deadlines.length - 1]
+                );
+                this.startDate = new Date(this.admins.startDate);
+                this.startCompleted = true;
+                this.stage = this.admins.stage;
+                for (let i = 0; i <= this.stage; i++) {
+                  this.dates.push(new Date(this.admins["deadlines"][i]));
+                  if (this.stage == 0 && i == 0) {
+                    const now = new Date();
+                    this.message = "Faculties add projects during this period";
+                    this.stageOne =
+                      Math.abs(now.getTime() - this.startDate.getTime()) /
+                      Math.abs(
+                        this.dates[0].getTime() - this.startDate.getTime()
+                      );
+                    this.stageOne = this.stageOne * 100;
+                    if (this.stageOne >= 100) {
+                      this.stageOneCompleted = true;
                     }
+                  }
+                  if (this.stage == 1 && i == 1) {
+                    this.stageOneCompleted = true;
+                    this.stageOne = 100;
+                    const now = new Date();
+                    this.stageTwo =
+                      Math.abs(now.getTime() - this.dates[0].getTime()) /
+                      Math.abs(
+                        this.dates[1].getTime() - this.dates[0].getTime()
+                      );
+                    this.stageTwo *= 100;
+                    if (this.stageTwo >= 100) {
+                      this.stageTwoCompleted = true;
+                    }
+                  }
+                  if (this.stage == 2 && i == 2) {
+                    this.stageOneCompleted = true;
+                    this.stageOne = 100;
+                    this.stageTwoCompleted = true;
+                    this.stageTwo = 100;
+                    const now = new Date();
+                    this.stageThree =
+                      Math.abs(now.getTime() - this.dates[1].getTime()) /
+                      Math.abs(
+                        this.dates[2].getTime() - this.dates[1].getTime()
+                      );
+                    this.stageThree *= 100;
+                    if (this.stageThree >= 100) {
+                      this.stageThreeCompleted = true;
+                    }
+                  }
+
+                  if (this.stage == 3 && i + 1 == 4) {
+                    this.stageThreeCompleted = true;
+                    this.stageThree = 100;
+                    this.stageOneCompleted = true;
+                    this.stageOne = 100;
+                    this.stageTwoCompleted = true;
+                    this.stageTwo = 100;
+                    const now = new Date();
+                    this.stageFour =
+                      Math.abs(now.getTime() - this.dates[2].getTime()) /
+                      Math.abs(
+                        this.dates[3].getTime() - this.dates[2].getTime()
+                      );
+                    this.stageFour *= 100;
+                    if (this.stageFour >= 100) {
+                      this.stageFourCompleted = true;
+                    }
+                  }
+                  if (this.stage == 4 && i == 4) {
+                    this.stageThreeCompleted = true;
+                    this.stageThree = 100;
+                    this.stageOneCompleted = true;
+                    this.stageOne = 100;
+                    this.stageTwoCompleted = true;
+                    this.stageTwo = 100;
+                    this.stageFourCompleted = true;
+                    this.stageFour = 100;
                   }
                 }
               }
-            });
+            }
+          }
         });
     }
   }
