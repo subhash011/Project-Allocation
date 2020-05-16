8bfcce37deb25b1a5ec1b6319236c5ed646bb391
subhash011 <subhash011011@gmail.com>
2020-05-16 12:38:55 +0530

    added test build

---
+++ btp-frontend/src/environments/environment.test.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiUrl: "http://localhost/",
+  GOOGLE_CLIENT_ID:
+    "1040090111157-llhk2n9egrpbv82tkijqm279q30s9mrk.apps.googleusercontent.com",
+};
