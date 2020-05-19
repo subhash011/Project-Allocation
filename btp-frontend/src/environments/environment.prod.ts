fbae4dfa2dd086902540ffe1ee87aa7ca107cdee
subhash011 <subhash011011@gmail.com>
2020-05-19 18:27:11 +0530

    changed test build, change evironment.prod

--- btp-frontend/src/environments/environment.prod.ts
+++ btp-frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: "https://pal.iitpkd.ac.in:10443/api",
+  apiUrl: "https://pal.iitpkd.ac.in:10443/api/",
   GOOGLE_CLIENT_ID:
     "230915389834-jni2e9hnq4ba86g6t5drr7f79c37gbai.apps.googleusercontent.com",
 };
