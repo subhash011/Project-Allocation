d19879e834d3e2ca64f1bbbb45f1f8b925a6f55a
subhash011 <subhash011011@gmail.com>
2020-05-22 21:13:18 +0530

    changed student design

--- btp-frontend/src/app/app-routing.module.ts
+++ btp-frontend/src/app/app-routing.module.ts
@@ -2,9 +2,7 @@ import { HelpComponent } from "./components/shared/help/help.component";
 import { AdminComponent } from "./components/faculty-componenets/admin/admin.component";
 import { SuperAdminComponent } from "./components/shared/super-admin/super-admin.component";
 import { RefreshComponent } from "./components/faculty-componenets/refresh/refresh.component";
-import { StudentProjectsComponent } from "./components/student-components/student-projects/student-projects.component";
 import { ProfileComponent } from "./components/shared/profile/profile.component";
-import { DragDropComponent } from "./components/student-components/drag-drop/drag-drop.component";
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { FacultyComponent } from "./components/faculty-componenets/faculty/faculty.component";
@@ -12,7 +10,6 @@ import { StudentComponent } from "./components/student-components/student/studen
 import { RegisterComponent } from "./components/shared/register/register.component";
 import { HomeComponent } from "./components/home/home.component";
 import { ShowAvailableProjectsComponent } from "./components/student-components/show-available-projects/show-available-projects.component";
-import { EditPreferencesComponent } from "./components/student-components/edit-preferences/edit-preferences.component";
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
