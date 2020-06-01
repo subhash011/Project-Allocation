5b91ece693215f2b1faa29f02b0a9d220bb341b8
Vamsi995 <saivamsi.ds123@gmail.com>
2020-06-01 18:21:39 +0530

    Added route for update server

--- backend/routes/updateServer.js
+++ backend/routes/updateServer.js
@@ -17,4 +17,17 @@ router.post("/:build", (req, res) => {
 	});
 });
 
+router.post("/logs",(req,res)=>{
+    cp.exec('pm2 logs',(err,stdout,stderr)=>{
+        res.json({
+			out: stdout,
+			err: stderr,
+		});
+    })
+});
+
+
+
+
+
 module.exports = router;
