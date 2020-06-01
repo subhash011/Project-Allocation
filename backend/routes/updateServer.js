b95a1fceb39827273e1f5cef83a99eb3c2b71968
subhash011 <subhash011011@gmail.com>
2020-06-01 18:06:48 +0530

    get error messages in script

--- backend/routes/updateServer.js
+++ backend/routes/updateServer.js
@@ -1,14 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const cp = require('child_process');
-const path = require('path')
+const cp = require("child_process");
+const path = require("path");
 
-router.post("/:build",(req,res)=>{
-    const param = req.params.build;
-    var file_path = path.resolve(__dirname, `../../Build-Script/build.sh ${param}`);
-    cp.exec(file_path,(err,stdout,stderr)=>{
-        res.send(stdout)
-    })
+router.post("/:build", (req, res) => {
+	const param = req.params.build;
+	var file_path = path.resolve(
+		__dirname,
+		`../../Build-Script/build.sh ${param}`
+	);
+	cp.exec(file_path, (err, stdout, stderr) => {
+		res.json({
+			out: stdout,
+			err: stderr,
+		});
+	});
 });
 
 module.exports = router;
