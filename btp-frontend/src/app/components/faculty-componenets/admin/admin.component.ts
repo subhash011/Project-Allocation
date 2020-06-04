d5fa3daa4730b961904847be7c121a38ac0d743c
Vamsi995 <saivamsi.ds123@gmail.com>
2020-06-04 13:52:15 +0530

    default sort

--- btp-frontend/src/app/components/faculty-componenets/admin/admin.component.ts
+++ btp-frontend/src/app/components/faculty-componenets/admin/admin.component.ts
@@ -1655,7 +1655,7 @@ export class AdminComponent implements OnInit, OnDestroy {
         case "Registered":
           return this.compare(a.isRegistered, b.isRegistered, isAsc);
         case "Email":
-          return this.compare(a.email.split("@")[0], b.email.split("@")[0], isAsc);
+          return this.compare(Number(a.email.split("@")[0]), Number(b.email.split("@")[0]), isAsc);
         default:
           return 0;
       }
