d5feb872aef0d13160eb9955d1e0408365aa766f
subhash011 <subhash011011@gmail.com>
2020-06-16 23:08:27 +0530

    fixed faculty part

--- btp-frontend/src/app/components/faculty-componenets/student-table/student-table.component.ts
+++ btp-frontend/src/app/components/faculty-componenets/student-table/student-table.component.ts
@@ -7,12 +7,22 @@ import {
   ViewChild,
   Pipe,
   PipeTransform,
+  OnChanges,
+  SimpleChanges,
 } from "@angular/core";
 import { CdkDragDrop, moveItemInArray } from "@angular/cdk/drag-drop";
 import { Router } from "@angular/router";
 import { Location } from "@angular/common";
 import { MatDialog } from "@angular/material/dialog";
 import { LoaderComponent } from "../../shared/loader/loader.component";
+import { MatTableDataSource } from '@angular/material';
+import {
+  trigger,
+  style,
+  state,
+  transition,
+  animate,
+} from "@angular/animations";
 
 @Pipe({
   name: "preference",
@@ -42,25 +52,45 @@ export class PreferencePipe implements PipeTransform {
   selector: "app-student-table",
   templateUrl: "./student-table.component.html",
   styleUrls: ["./student-table.component.scss"],
+  animations: [
+    trigger("detailExpand", [
+      state(
+        "collapsed",
+        style({ height: "0px", minHeight: "0", display: "none" })
+      ),
+      state("expanded", style({ height: "*" })),
+      transition(
+        "expanded <=> collapsed",
+        animate("0ms cubic-bezier(0.4, 0.0, 0.2, 1)")
+      ),
+    ]),
+  ],
 })
-export class StudentTableComponent implements OnInit {
+export class StudentTableComponent implements OnInit, OnChanges {
   @Input() public student_list;
   @Input() public project;
   @Input() public adminStage;
 
   @ViewChild("table", { static: false }) table;
 
-  public fields = ["Name", "CGPA", "Roll"];
-
+  public fields = ["Name", "CGPA", "Roll","Index", "Actions"];
+  students:MatTableDataSource<any>;
+  expandedElement:any;
   constructor(
     private projectService: ProjectsService,
-    private location: Location,
-    private router: Router,
     private snackBar: MatSnackBar,
     private dialog: MatDialog
   ) {}
 
-  ngOnInit() {}
+  ngOnChanges(simpleChanges:SimpleChanges){
+    if(simpleChanges.student_list) {
+      this.students = new MatTableDataSource(this.student_list);
+    }
+  }
+
+  ngOnInit() {
+    this.students = new MatTableDataSource(this.student_list);
+  }
 
   onSubmit() {
     if (this.adminStage == 2) {
@@ -126,38 +156,44 @@ export class StudentTableComponent implements OnInit {
   }
 
   drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.student_list, event.previousIndex, event.currentIndex);
+    let previousIndex = this.students.data.findIndex(
+      (row) => row === event.item.data
+    );
+    moveItemInArray(this.students.data, previousIndex, event.currentIndex);
+    this.students.data = [...this.students.data];
   }
 
-  moveToTop(project){
-
-    this.student_list = this.student_list.filter((val) => {
-      return val._id != project._id;
+  moveToTop(student){
+    this.students.data = this.students.data.filter((val) => {
+      return val._id != student._id;
     });
-    this.student_list.unshift(project);
+    this.students.data.unshift(student);
+    this.students.data = [...this.students.data];
   }
 
   moveToBottom(project){
 
-    this.student_list = this.student_list.filter((val) => {
+    this.students.data = this.students.data.filter((val) => {
       return val._id != project._id;
     });
-    this.student_list.push(project);
-
+    this.students.data.push(project);
+    this.students.data = [...this.students.data];
   }
 
   moveOneUp(index){
     if (index == 0) {
       return;
     }
-    moveItemInArray(this.student_list, index, index - 1);
+    moveItemInArray(this.students.data, index, index - 1);
+    this.students.data = [...this.students.data];
   }
 
   moveOneDown(index){
-    if (index == this.student_list.length - 1) {
+    if (index == this.students.data.length - 1) {
       return;
     }
-    moveItemInArray(this.student_list, index, index + 1);
+    moveItemInArray(this.students.data, index, index + 1);
+    this.students.data = [...this.students.data];
   }
 
 }
