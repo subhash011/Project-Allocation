dbfe5f701334b97c06bd5f4ce2dce724ebe3e3e2
subhash011 <subhash011011@gmail.com>
2020-06-17 14:52:00 +0530

    webhook

--- backend/routes/updateServer.js
+++ backend/routes/updateServer.js
@@ -47,7 +47,7 @@ async function executeScript(comm) {
 router.post("/",verifyPostData, (req, res) => {
 	const parameter = "yes";
 	// res.status(200).send("success");
-	// req.setTimeout(300000);
+	req.setTimeout(300000);
 	// res.setTimeout(300000);
 	var file_path = path.resolve(
 		__dirname,
