beab242971865b4254c718549b1e4d1b3ff487b4
subhash011 <subhash011011@gmail.com>
2020-06-20 16:16:58 +0530

    update backup to get exact data

--- backend/routes/backup.js
+++ backend/routes/backup.js
@@ -10,43 +10,43 @@ const Streams = require("../models/Streams");
 
 
 router.get(`/${process.env.SECRET_KEY}/streams`,(req,res) => {
-    Streams.find().lean().select("-__v -_id").then(streams => {
+    Streams.find().lean().select("-__v").then(streams => {
         res.send(streams);
     })
 });
 
 router.get(`/${process.env.SECRET_KEY}/projects`,(req,res) => {
-    Project.find().lean().select("-_id -__v").then(streams => {
+    Project.find().lean().select("-__v").then(streams => {
         res.send(streams);
     })
 });
 
 router.get(`/${process.env.SECRET_KEY}/students`,(req,res) => {
-    Student.find().lean().select("-google_id -_id -__v -date").then(streams => {
+    Student.find().lean().select("-google_id -__v -date").then(streams => {
         res.send(streams);
     })
 })
 
 router.get(`/${process.env.SECRET_KEY}/faculty`,(req,res) => {
-    Faculty.find().lean().select("-google_id -_id -__v -date").then(streams => {
+    Faculty.find().lean().select("-google_id -__v -date").then(streams => {
         res.send(streams);
     })
 })
 
 router.get(`/${process.env.SECRET_KEY}/super`,(req,res) => {
-    SuperAdmin.find().lean().select("-google_id -_id -__v").then(streams => {
+    SuperAdmin.find().lean().select("-google_id -__v").then(streams => {
         res.send(streams);
     })
 })
 
 router.get(`/${process.env.SECRET_KEY}/admin`,(req,res) => {
-    Admin.find().lean().select("-_id -__v").then(streams => {
+    Admin.find().lean().select("-__v").then(streams => {
         res.send(streams);
     })
 })
 
 router.get(`/${process.env.SECRET_KEY}/mappings`,(req,res) => {
-    Mapping.find().lean().select("-_id -__v").then(streams => {
+    Mapping.find().lean().select("-__v").then(streams => {
         res.send(streams);
     })
 })
