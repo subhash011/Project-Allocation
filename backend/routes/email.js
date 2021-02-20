fd01a884a383e163dc71892a7b94dd77b8dcba71
subhash011 <subhash011011@gmail.com>
2021-02-20 20:50:40 +0530

    uncommented email.js

--- backend/routes/email.js
+++ backend/routes/email.js
@@ -12,51 +12,45 @@ sendEmail = (mailOptions, transporter) =>
             resolve();
         });
     });
+
 router.post("/send", async (req, res) => {
-    res.status(200).json({
-        statusCode: 200,
-        message: "Mails have been sent.",
-        result: {
-            sent: true
-        }
-    });
-    // try {
-    //     const to = req.body.to;
-    //     const subject = req.body.subject;
-    //     const mailBody = req.body.mailBody;
-    //     const options = {
-    //         bcc: to,
-    //         subject: subject,
-    //         text: mailBody,
-    //         replyTo: "Do not reply to this email pap@smail.iitpkd.ac.in"
-    //     };
-    //     const transport = nodemailer.createTransport({
-    //         host: "smtp.gmail.com",
-    //         port: 465,
-    //         secure: true,
-    //         auth: {
-    //             user: process.env.PAP_MAIL,
-    //             pass: process.env.PAP_PASS
-    //         }
-    //     });
-    //     await sendEmail(options, transport);
-    //     res.status(200).json({
-    //         statusCode: 200,
-    //         message: "Mails have been sent.",
-    //         result: {
-    //             sent: true
-    //         }
-    //     });
-    // } catch (e) {
-    //     res.status(500).json({
-    //         statusCode: 500,
-    //         message: "Internal Server Error! Please Sign-In again.",
-    //         result: {
-    //             sent: false,
-    //             error: e
-    //         }
-    //     });
-    // }
+    try {
+        const to = req.body.to;
+        const subject = req.body.subject;
+        const mailBody = req.body.mailBody;
+        const options = {
+            bcc: to,
+            subject: subject,
+            text: mailBody,
+            replyTo: "Do not reply to this email pap@smail.iitpkd.ac.in"
+        };
+        const transport = nodemailer.createTransport({
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true,
+            auth: {
+                user: process.env.PAP_MAIL,
+                pass: process.env.PAP_PASS
+            }
+        });
+        await sendEmail(options, transport);
+        res.status(200).json({
+            statusCode: 200,
+            message: "Mails have been sent.",
+            result: {
+                sent: true
+            }
+        });
+    } catch (e) {
+        res.status(500).json({
+            statusCode: 500,
+            message: "Internal Server Error! Please Sign-In again.",
+            result: {
+                sent: false,
+                error: e
+            }
+        });
+    }
 });
 
 module.exports = router;
